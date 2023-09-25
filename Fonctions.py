e19f52565434ae4ee2becb8007e6e87f26f51c87
François-Régis <francois-regis.drutel@cpe.fr>
2023-09-25 17:44:07 +0200

    oui

--- Fonctions.py
+++ Fonctions.py
@@ -3,48 +3,32 @@
 Fichier des fonctions
 25/09/2023
 François-Régis DRUTEL, Paul DUMONT
-
+il reste à perfectionner la fonction d'analyse et d'afficher 
+si la phrase est syntaxiquement correct
 """
-#acquisition de la phrase à analyser et transformation en liste d'états
+
 def acquiphrase ():
+#acquisition de la phrase à analyser    
     phrase = str(input("entrez votre phrase : "))
     phrase = phrase.split()
     return phrase
 
-
-dictionnaire = {"le" : 0, "la" : 0, "chat" : 2, "souris" : 2, "Martin" : 4,
-"mange" : 3, "la" : 0, "petite" : 1, "joli" : 1, "grosse" : 1,
-"bleu" : 1, "verte" : 1, "dort" : 3,"julie" : 4, "Jean" : 4, "." : 5}      
-
-
-dicoetat={
-    0 : [4,1,8,8,8,8],
-    1 : [8,8,1,2,8,8],
-    2 : [8,8,2,8,3,8],
-    3 : [7,5,8,8,8,9],
-    4 : [8,8,8,8,3,8],
-    5 : [8,8,5,6,8,8],
-    6 : [8,8,6,8,8,9],
-    7 : [8,8,8,8,8,9], 
-    8 : [8,8,8,8,8,8],
-    9 : [9,9,9,9,9,9]
-    }
-
-
-def analyse(phrase):
-    etat = 0
-    while etat != 8:
-        for mot in phrase:
-            etat = dicoetat[etat][dictionnaire[mot]]
-
-    return etat
-
 def conversion(phrase):
+# transformation de la phrase en liste de chiffre qui représente l'état
     etat = []
     for mots in phrase :
         if mots in dictionnaire :
             etat.append(dictionnaire[mots])
     return etat
-            
-phrase = acquiphrase()
-print(conversion(phrase))
+
+def analyse(dico,phrase):
+#pour chaque valeur de notre liste de chiffre on va définir l'etat correspondant en fonction de notre dicoetat 
+#notre fonction s'arrete après le dernier état et on regarde si la phrase et correcte ou non (8 ou 9)
+    etat=0
+    for i in phrase:
+        
+        etat=dico[etat][i]
+        print(etat)
+    
+        if etat in [8, 9]:
+            return etat == 9
--- Programme_principal.py
+++ Programme_principal.py
@@ -1,6 +1,29 @@
 #Header
 """
+programme principal
+25/09/2023
+François-Régis DRUTEL, Paul DUMONT
 
 """
+from Fonctions import acquiphrase, conversion, analyse
 
-from Fonctions import acquiphrase
+dictionnaire = {"le" : 0, "la" : 0, "chat" : 2, "souris" : 2, "Martin" : 4,
+"mange" : 3, "la" : 0, "petite" : 1, "joli" : 1, "grosse" : 1,
+"bleu" : 1, "verte" : 1, "dort" : 3,"julie" : 4, "Jean" : 4, "." : 5}      
+
+
+dicoetat={
+    0 : [4,1,8,8,8,8],
+    1 : [8,8,1,2,8,8],
+    2 : [8,8,2,8,3,8],
+    3 : [7,5,8,8,8,9],
+    4 : [8,8,8,8,3,8],
+    5 : [8,8,5,6,8,8],
+    6 : [8,8,6,8,8,9],
+    7 : [8,8,8,8,8,9], 
+    8 : [8,8,8,8,8,8],
+    9 : [9,9,9,9,9,9]
+    }
+
+#test pour voir si la conversion est efficace
+phrase = acquiphrase()
